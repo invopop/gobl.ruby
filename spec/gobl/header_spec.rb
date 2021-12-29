4f1ee9f5e8b8ec53d5957e11cdb572ecb5e92960
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2021-12-29 10:36:49 +0000

    Header meta attribute test

---
+++ spec/gobl/header_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/gobl'
+
+RSpec.describe GOBL::Header do
+  it 'creates a new header from a JSON' do
+    gobl = File.read('spec/example/basic_header.json')
+    header = described_class.from_json!(gobl)
+
+    expect(header.header_meta['meta_tag_a']).to eq('a')
+  end
+end
