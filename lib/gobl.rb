65ebacc5764f2e82696a58da35eea3e658874654
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2021-12-29 11:08:21 +0000

    Moved 'fetch_object' method to the 'Envelope' object

--- lib/gobl.rb
+++ lib/gobl.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'zeitwerk'
 
 # GOBL - Main namespace which has the differents structures to generate and
@@ -22,18 +23,6 @@ module GOBL
     loader.inflector.inflect(inflections)
     loader
   end
-
-  # Returns a Ruby class for a given JSON schema definition name.
-  def self.fetch_object(type)
-    case type
-    when 'bill.Invoice'
-      GOBL::Bill::Invoice
-    when 'note.Message'
-      GOBL::Note::Message
-    else
-      raise 'envelope type not recognized'
-    end
-  end
 end
 
 GOBL.loader.setup
@@ -42,12 +31,24 @@ module GOBL
   # Envelope 'from_json!' method overrides the auto generated definition to
   # take into account the envelope type.
   class Envelope
+    # Returns a GoBL object for a given envelope type.
+    def self.fetch_object(type)
+      case type
+      when 'bill.Invoice'
+        GOBL::Bill::Invoice
+      when 'note.Message'
+        GOBL::Note::Message
+      else
+        raise 'envelope type not recognized'
+      end
+    end
+
     def self.from_json!(json)
       return if json.nil?
 
       instance = from_gobl!(JSON.parse(json))
 
-      kls = GOBL.fetch_object(instance.head.typ)
+      kls = fetch_object(instance.head.typ)
       instance.attributes[:doc] = kls.from_gobl!(instance.doc.value)
 
       instance
