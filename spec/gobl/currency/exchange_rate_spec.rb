ee5fbf5914811b4432034f6ffc7efc3628a36e64
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2022-01-25 17:42:59 +0000

    Tests fixed

--- spec/gobl/currency/exchange_rate_spec.rb
+++ spec/gobl/currency/exchange_rate_spec.rb
@@ -4,14 +4,14 @@ require_relative '../../../lib/gobl'
 
 RSpec.describe GOBL::Currency::ExchangeRate do
   it 'creates a new exchange rate from a JSON' do
-    obj = { 'currency' => 'EUR', 'value' => '100' }
+    obj = { 'currency' => 'EUR', 'amount' => '100' }
     xchg = described_class.from_json!(obj.to_json)
 
-    expect(xchg.value.value).to eq('100')
+    expect(xchg.amount.value).to eq('100')
   end
 
   it 'converts an exchange rate object into a JSON' do
-    json = { 'currency' => 'EUR', 'value' => '100' }.to_json
+    json = { 'currency' => 'EUR', 'amount' => '100' }.to_json
     xchg = described_class.from_json!(json)
 
     expect(xchg.to_json).to eq(json)
