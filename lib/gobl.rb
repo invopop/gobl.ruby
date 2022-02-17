c1c89c3e172f4707e58537efe71871274cf7cf7c
Sam Lown <sam.lown@invopop.com>
2022-02-17 23:26:35 +0000

    Moving types to top-level

--- lib/gobl.rb
+++ lib/gobl.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'zeitwerk'
+require 'dry-types'
 
 # Main GOBL namespace which has the differents structures to generate and
 # load its components. There are sub-namespaces specically defined for GoBL
@@ -26,6 +27,18 @@ module GOBL
     loader = Zeitwerk::Loader.for_gem
     loader.inflector.inflect(inflections)
     loader
+  end
+
+  # Types used by the generated GOBL code.
+  module Types
+    include Dry.Types()
+
+    Nil    = Strict::Nil
+    Int    = Strict::Integer
+    Bool   = Strict::Bool
+    Hash   = Strict::Hash
+    String = Strict::String
+    Double = Strict::Float | Strict::Integer
   end
 end
 
