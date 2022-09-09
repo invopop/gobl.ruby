d823de6b1460cceee5ca9374b2bbcf4c2b96304e
Luismi Cavallé <luismi@lmcavalle.com>
2022-09-09 13:26:20 +0000

    Reorganise and simplify `lib/gobl.rb`

--- lib/gobl.rb
+++ lib/gobl.rb
@@ -4,56 +4,32 @@ require 'json'
 require 'zeitwerk'
 require 'dry-struct'
 require 'active_support/core_ext/string/inflections'
-require 'active_support/concern'
 require 'forwardable'
 require 'net/http'
 require 'base64'
+
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect(
+  'gobl' => 'GOBL',
+  'gobl_extensions' => 'GOBLExtensions',
+  'uuid' => 'UUID',
+  'url' => 'URL',
+  'item_id' => 'ItemID',
+  'tax_id' => 'TaxID',
+  'dsig' => 'DSig',
+  'id' => 'ID'
+)
+loader.setup
 
 # Main GOBL namespace which has the differents structures to generate and
 # load its components. There are sub-namespaces specically defined for GOBL
 # objects, based on the GOBL JSON schema.
 module GOBL
-  def self.inflections
-    {
-      'gobl' => 'GOBL',
-      'gobl_extensions' => 'GOBLExtensions',
-      'uuid' => 'UUID',
-      'url' => 'URL',
-      'item_id' => 'ItemID',
-      'tax_id' => 'TaxID',
-      'dsig' => 'DSig'
-    }
-  end
-
-  def self.loader
-    @loader ||= create_loader
-  end
-
-  def self.create_loader
-    loader = Zeitwerk::Loader.for_gem
-    loader.inflector.inflect(inflections)
-    loader
-  end
+  extend GOBL::Operations
 
   def self.config
     @config ||= Config.new
   end
 end
 
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.acronym 'GOBL'
-  inflect.acronym 'UUID'
-  inflect.acronym 'URL'
-  inflect.acronym 'DSig'
-  # inflect.acronym 'ItemID'
-  # inflect.acronym 'TaxID'
-end
-
-require_relative 'types'
-require_relative 'id'
-
-GOBL.loader.setup
-
 require_relative 'extensions'
-
-GOBL.extend GOBL::Operations
