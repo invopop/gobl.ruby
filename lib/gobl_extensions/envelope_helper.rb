dfb03bf1c33e831102e94905c13a80b08360391c
Luismi Cavallé <luismi@lmcavalle.com>
2022-09-20 10:17:08 +0000

    Remove unreachable method

--- lib/gobl_extensions/envelope_helper.rb
+++ lib/gobl_extensions/envelope_helper.rb
@@ -1,10 +1,6 @@
 module GOBLExtensions
+  # Additional methods for the generated {GOBL::Envelope} class
   module EnvelopeHelper
-    # Provide the GOBL Envelope's schema, which will also contain version information.
-    def schema
-      @schema ||= GOBL::ID.new(value['$schema'])
-    end
-
     def extract
       doc.extract
     end
